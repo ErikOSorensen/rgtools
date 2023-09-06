6b6b7ee5a4db6427b461cc0d97c3723ab548a0b8
gufranpathan <gufranpathan@outlook.com>
2023-09-05 17:52:02 -0700

    fix typo

--- src/rgtools/xml_processing/generate_latex.py
+++ src/rgtools/xml_processing/generate_latex.py
@@ -137,7 +137,7 @@ class XMLToLatex:
 		# 	self.add_to_latex(self.itemize_dict(arm))
 
 	def add_armsgroups(self):
-		self.pandas_to_latex(self.xml_processor.armgroups_df[['label','armblabel']],
+		self.pandas_to_latex(self.xml_processor.armgroups_df[['label','armlabel']],
 							 label_col="label",
 							 column_format="p{0.1cm}p{3.5cm}X",
 							 caption="Arm Groups")
